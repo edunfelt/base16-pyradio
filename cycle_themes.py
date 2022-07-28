18f8f561022dd3ef2d3e955a07f7390fcc76b411
Spiros Georgaras <sng@hellug.gr>
2022-07-28 16:28:38 +0300

    py scripts in repo

--- cycle_themes.py
+++ cycle_themes.py
@@ -238,7 +238,14 @@ copy_thread.start()
 
 while True:
     char = scr.getch()
-    if char == ord('q'):
+    if char == curses.KEY_RESIZE:
+        ny, nx = scr.getmaxyx()
+        win.resize(ny-15, nx)
+        win.mvwin(15, 0)
+        # win.touchwin()
+        # win.redrawwin()
+        # win.refresh()
+    elif char == ord('q'):
         stop_thread = True
         break
     elif char in (ord('+'), ord('.')):
